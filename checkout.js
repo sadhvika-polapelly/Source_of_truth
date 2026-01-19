7cba1073a4960d842611fb878d617f86894794ca
sadhvika-polapelly <sadhvikapolapelly120@gmail.com>
2026-01-19 10:54:38 +0530

    use calculateTax function in checkout.js

--- checkout.js
+++ checkout.js
@@ -1,3 +1,8 @@
 function checkout(amount) {
-    return amount * 1.0; // placeholder calculation
+    return amount * 1.18; // placeholder calculation
+}
+
+function checkout(amount) {
+     const price=calculateTax(amount);
+     return price;
 }
